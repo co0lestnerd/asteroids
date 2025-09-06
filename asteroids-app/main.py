0930a55368aabdb97105f3cdcabd382d0e3d14f9
co0lestnerd <overlooked723@gmail.com>
2025-09-06 18:21:04 +1000

    Create and draw Player sprite

--- asteroids-app/main.py
+++ asteroids-app/main.py
@@ -1,16 +1,20 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
 
+    #set up screen
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    #define colours
-    black = (0, 0, 0)
+    #set up clock
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     
     #game loop
     while True:
@@ -19,12 +23,14 @@ def main():
                 return
         
         # Fill screen with black
-        screen.fill(black)
-
+        screen.fill("black")
+        # Re-render the player
+        player.draw(screen)
         # Update the display
         pygame.display.flip()
 
-
+        # Update the clock and limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
