e7f5fbf32ccf1f77470a93dab61758e22bc7b5d3
co0lestnerd <overlooked723@gmail.com>
2025-09-10 17:32:23 +1000

    Added movement to Player and created grouping

--- asteroids-app/main.py
+++ asteroids-app/main.py
@@ -11,6 +11,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #set up groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     #set up clock
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -22,13 +27,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        # Update the player
-        player.update(dt)
+        # Update the player/all updatables
+        updatable.update(dt)
         
         # Fill screen with black
         screen.fill("black")
-        # Re-render the player
-        player.draw(screen)
+        # Re-render the player/loop over drawbable to draw them all
+        for objects in drawable:
+            objects.draw(screen)
         # Update the display
         pygame.display.flip()
 
