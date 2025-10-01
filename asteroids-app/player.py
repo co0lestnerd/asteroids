a0d158e70a0a6badb07a8bf57818881cfeb49bb2
co0lestnerd <overlooked723@gmail.com>
2025-10-01 17:53:51 +1000

    Added cooldown to shooting

--- asteroids-app/player.py
+++ asteroids-app/player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)  
@@ -21,6 +22,7 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_timer -= dt
 
         if keys[pygame.K_w]:
             self.move(dt)
@@ -41,5 +43,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shot_timer > 0:
+            return
         newshot = Shot(self.position.x, self.position.y)
         newshot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
